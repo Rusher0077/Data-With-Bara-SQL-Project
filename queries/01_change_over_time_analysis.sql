769cac32ab11bdc015b07d65bffccce57ec46f23
Pallab Dey <pallabdey21@gmail.com>
2026-02-24 23:23:45 +0600

    doc: capitalize keyword and fix indentation

--- queries/01_change_over_time_analysis.sql
+++ queries/01_change_over_time_analysis.sql
@@ -1,5 +1,5 @@
 -- 01 - Changes over time analysis
--- Analyse sales performance over time
+-- Analyse performance over time
 
 ------------------------------------------------------------------------
 
@@ -8,13 +8,13 @@
 -- Displaying just the year of order date. More cleaner
 
 SELECT
-    EXTRACT(YEAR from order_date) as order_year,
+    EXTRACT(YEAR FROM order_date) AS order_year,
     SUM(sales_amount) AS total_sales,
-	count(DISTINCT customer_key) as total_customer
+    COUNT(DISTINCT customer_key) AS total_customer
 FROM gold.fact_sales
 WHERE order_date IS NOT NULL
-GROUP BY EXTRACT(YEAR from order_date)
-ORDER BY EXTRACT(YEAR from order_date)
+GROUP BY EXTRACT(YEAR FROM order_date)
+ORDER BY EXTRACT(YEAR FROM order_date);
 
 ------------------------------------------------------------------------
 
@@ -30,20 +30,20 @@ SELECT
 FROM gold.fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY DATE_TRUNC('year', order_date)::date
-ORDER BY DATE_TRUNC('year', order_date)::date
+ORDER BY DATE_TRUNC('year', order_date)::date;
 
 ------------------------------------------------------------------------
 
 -- Shows the monthly order from its first day. More granular
 
-SELECT 
-	date_trunc('month', order_date)::date as order_year,
-	sum(sales_amount) as total_sales,
-	count(distinct customer_key) as total_customers
+SELECT
+    DATE_TRUNC('month', order_date)::date AS order_year,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers
 FROM gold.fact_sales
-WHERE order_date is not null
-GROUP by date_trunc('month', order_date)::date
-order by date_trunc('month', order_date)::date
+WHERE order_date IS NOT NULL
+GROUP BY DATE_TRUNC('month', order_date)::date
+ORDER BY DATE_TRUNC('month', order_date)::date;
 
 ------------------------------------------------------------------------
 
@@ -52,11 +52,11 @@ order by date_trunc('month', order_date)::date
 -- Shows date in Year-Month format but the month will not get sorted properly 
 -- because it is string
 
-select 
-	to_char(order_date, 'yyyy-Mon') as order_year,
-	sum(sales_amount) as total_sales,
-	count(distinct customer_key) as total_customers
+SELECT
+    TO_CHAR(order_date, 'yyyy-Mon') AS order_year,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers
 FROM gold.fact_sales
-WHERE order_date is not null
-GROUP by to_char(order_date, 'yyyy-Mon')
-order by to_char(order_date, 'yyyy-Mon')
+WHERE order_date IS NOT NULL
+GROUP BY TO_CHAR(order_date, 'yyyy-Mon')
+ORDER BY TO_CHAR(order_date, 'yyyy-Mon');
